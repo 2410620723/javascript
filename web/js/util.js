937ae4560f3cc6a0d46760254979723f41597eb5
2410620723 <2410620723@qq.com>
2017-10-31 14:52:28 +0800

    获取元素距离body的上边距左边距

--- web/js/util.js
+++ web/js/util.js
@@ -15,5 +15,28 @@ var utils = {
     },
     parseJSON:function (str) {
         return "JSON" in window?JSON.parse(str):eval("("+str+")");
+    },
+    offset:function (curEle) {
+        var totalLeft = null, totalTop = null, par = curEle.offsetParent;
+        totalLeft += curEle.offsetLeft;
+        totalTop += curEle.offsetTop;
+        while (par) {
+            //累加父级参照物的边框
+            if (navigator.userAgent.indexOf("MSIE 8.0") === -1){
+                totalLeft += par.clientLeft;
+                totalTop += par.clientTop;
+            }
+            totalLeft += par.offsetLeft;
+            totalTop += par.offsetTop;
+            par = par.offsetParent;
+        }
+        return {left: totalLeft, top: totalTop};
+    },
+    win:function (attr, value) {
+        if (typeof value === "undefined") {
+            return document.documentElement[attr] || document.body[attr];
+        }
+        document.documentElement[attr] = value;
+        document.body[attr] = value;
     }
 };
