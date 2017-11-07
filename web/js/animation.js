41d917f3b41c6d9cf5721d290286b3c301aedec9
2410620723 <2410620723@qq.com>
2017-11-07 16:47:11 +0800

    动画库

---
+++ web/js/animation.js
@@ -0,0 +1,39 @@
+/*
+ * 动画库
+ * Created by zxm on 2017-11-7.
+ */
+~function () {
+    var effect = {
+        linear: function (currentTime, begin, total, duration) {
+            return currentTime / duration * total + begin;
+        }
+    };
+    function move(curEle, target, duration, callback) {
+        clearInterval(curEle.timer);
+        var begin = {}, change = {};
+        for (var key in target) {
+            // 不需要遍历那些共有的属性
+            if (target.hasOwnProperty(key)) {
+                begin[key] = utils.css(curEle, key);
+                change[key] = target[key] - begin[key];
+            }
+        }
+        var time = 0;
+        curEle.timer = setInterval(function () {
+            time += 10;
+            if (time >= duration) {
+                utils.css(curEle, target);
+                callback && callback.call(curEle);
+                clearInterval(curEle.timer);
+                return;
+            }
+            for (var key in target) {
+                if (target.hasOwnProperty(key)) {
+                    var curPos = effect.linear(time, begin[key], change[key], duration);
+                    utils.css(curEle, key, curPos);
+                }
+            }
+        },10);
+    }
+    window.myAnimate = move;
+}();
